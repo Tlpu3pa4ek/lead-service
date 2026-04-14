a4e6726bb0010048bc4980d4d2ad65276c6fd5c8
Tlpu3pa4ek <121636869+Tlpu3pa4ek@users.noreply.github.com>
2026-04-14 17:19:11 +0500

    Update sheets.service.js

--- src/services/sheets.service.js
+++ src/services/sheets.service.js
@@ -1,6 +1,6 @@
 import path from 'path';
-const { google } from 'googleapis';
-const { fileURLToPath } from 'url';
+import { google } from 'googleapis';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
